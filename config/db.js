6da608e41e3c51c2ac94bc9d1c20ad493817aaa5
Abhishek Srivastav <abhisrivastav551@gmail.com>
2020-06-21 12:21:06 +0530

    a

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose'); 
-
+//"mongodb+srv://user-abhi:cluster-abhi@cluster1-abhi-qjpjx.mongodb.net/Cluster1-Abhi?retryWrites=true&w=majority"
 const connectDB = async () => {
   try {
-    await mongoose.connect( MONGOLAB_URI , {
+    await mongoose.connect( "mongodb+srv://user-abhi:cluster-abhi@cluster1-abhi-qjpjx.mongodb.net/test?authSource=admin&replicaSet=Cluster1-Abhi-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
